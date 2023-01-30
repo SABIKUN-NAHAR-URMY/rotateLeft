5e8ff41fb405f0b5a745dbdf170c91d180258806
SABIKUN-NAHAR-URMY <snurmy18@gmail.com>
2023-01-30 14:25:58 +0600

    code done

--- rotateLeft.js
+++ rotateLeft.js
@@ -1,9 +1,9 @@
-function rotateLeft(arr, d){
-    for(let i = 0; i < d; i++){
+function rotateLeft(arrayOfNumber, numberInt){
+    for(let i = 0; i < numberInt; i++){
         arr.push(arr.shift());
     }
     return arr;
 }
 
-const arr = [1,2,3,4,5,6,7];
-console.log(rotateLeft(arr, 4));
+const arrayOfNumber = [1,2,3,4,5];
+console.log(rotateLeft(arrayOfNumber, 2));
